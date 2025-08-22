9582431d763742b93ff7293a3dd62463dd19fc83
katysh-aa <jogatv32@gmail.com>
2025-08-22 14:21:53 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+// sw.js
+const CACHE_NAME = 'family-budget-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/app.js',
+    '/style.css',
+    '/icons/icon-192x192.png',
+    '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
